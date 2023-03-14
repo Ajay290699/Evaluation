ba05d468a67eaf162d3fca78570fb042e043aafc
Ajay Chavan <ajayc9838@gmail.com>
2023-03-14 11:06:14 +0530

    added scanner for user input

--- src/Recursion/MultiplicatioByRecursive.java
+++ src/Recursion/MultiplicatioByRecursive.java
@@ -1,18 +1,22 @@
 package Recursion;
 
+import java.util.Scanner;
+
 public class MultiplicatioByRecursive {
 
     public static void main(String[] args) {
-        int num = 7;
-        int i = 1;
-        multiplication(num, i);
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter a number which you want a multipication table :");
+        int num = sc.nextInt();
+        multiplication(num, 1);
 
     }
 
-    static void multiplication(int num, int i) {
+    public static void multiplication(int num, int i) {
         if (i > 10)
             return;
         System.out.println(num + " * " + i + " = " + num * i);
         multiplication(num, i + 1);
+
     }
 }
