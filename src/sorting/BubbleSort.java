0417b75afa8bef025ff50804e2d31b94c8461cb1
Ajay Chavan <ajayc9838@gmail.com>
2023-03-20 13:28:20 +0530

    intial

--- src/sorting/BubbleSort.java
+++ src/sorting/BubbleSort.java
@@ -1,6 +1,7 @@
 package sorting;
 
 public class BubbleSort {
+    
     public static void main(String[] args) {
         int[] arr = {7, 6, 9, 10, 2, 5, 3, 1};
         for (int i = 0; i < arr.length - 1; i++) {
